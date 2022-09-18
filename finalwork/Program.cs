25b77f52844993b430a629443bcf1a99c741d7cc
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-09-18 10:36:47 +0300

    добавлен первый метод программы

--- finalwork/Program.cs
+++ finalwork/Program.cs
@@ -7,3 +7,11 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 // 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
